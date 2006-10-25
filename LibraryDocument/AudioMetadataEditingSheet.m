e5f1a659897e37f030aa658bd71e422ecea1370d
stephen_booth <stephen_booth@localhost>
2006-10-25 15:28:14 +0000

    Added

---
+++ LibraryDocument/AudioMetadataEditingSheet.m
@@ -0,0 +1,64 @@
+/*
+ *  $Id$
+ *
+ *  Copyright (C) 2006 Stephen F. Booth <me@sbooth.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import "AudioMetadataEditingSheet.h"
+#import "Genres.h"
+
+@implementation AudioMetadataEditingSheet
+
+- (id) init
+{
+	if((self = [super init])) {
+		BOOL		result;
+		
+		result		= [NSBundle loadNibNamed:@"AudioMetadataEditingSheet" owner:self];
+		if(NO == result) {
+			NSLog(@"Missing resource: \"AudioMetadataEditingSheet.nib\".");
+			[self release];
+			return nil;
+		}
+		
+		return self;
+	}
+	
+	return nil;
+}
+
+- (NSWindow *) sheet
+{
+	return [[_sheet retain] autorelease];
+}
+
+- (IBAction) ok:(id)sender
+{
+    [[NSApplication sharedApplication] endSheet:[self sheet] returnCode:NSOKButton];
+}
+
+- (IBAction) cancel:(id)sender
+{
+    [[NSApplication sharedApplication] endSheet:[self sheet] returnCode:NSCancelButton];
+}
+
+- (NSArray *) genres
+{
+	return [Genres sharedGenres];
+}
+
+@end
---
+++ LibraryDocument/LibraryDocumentWindow.m
@@ -0,0 +1,45 @@
+/*
+ *  $Id$
+ *
+ *  Copyright (C) 2006 Stephen F. Booth <me@sbooth.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import "LibraryDocumentWindow.h"
+#import "LibraryDocument.h"
+
+@implementation LibraryDocumentWindow
+
+- (void) keyDown:(NSEvent *)event
+{
+	unichar			key		= [[event charactersIgnoringModifiers] characterAtIndex:0];    
+	unsigned int	flags	= [event modifierFlags] & 0x00FF;
+		
+	if((0x0020 == key || NSCarriageReturnCharacter == key ) && 0 == flags) {
+		[(LibraryDocument *)[[self windowController] document] playPause:self];
+	}
+	else if(0xf702 == key && 0 == flags) {
+		[(LibraryDocument *)[[self windowController] document] skipBackward:self];
+	}
+	else if(0xf703 == key && 0 == flags) {
+		[(LibraryDocument *)[[self windowController] document] skipForward:self];
+	}
+	else {
+		[super keyDown:event]; // let somebody else handle the event 
+	}
+}
+
+@end
