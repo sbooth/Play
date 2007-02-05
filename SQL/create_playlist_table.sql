a4ffe8a21d8526e062fb020c7e2f47016a4a1a39
stephen_booth <stephen_booth@localhost>
2007-02-05 18:17:11 +0000

    Updated table definition

--- SQL/create_playlist_table.sql
+++ SQL/create_playlist_table.sql
@@ -1,10 +1,9 @@
-## The playlist table
 CREATE TABLE IF NOT EXISTS 'playlists' (
 
 	'id' 					INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	'name' 					TEXT,
+	'type'					INTEGER,
 
-	## General playlist information
 	'date_added' 			REAL,
 	'first_played_date' 	REAL,
 	'last_played_date' 		REAL
