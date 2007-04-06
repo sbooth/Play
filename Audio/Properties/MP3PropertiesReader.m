62fccafa35f37fc885251fdd7cef65ee7e1aeeea
stephen_booth <stephen_booth@localhost>
2007-04-06 13:05:26 +0000

    Use MAD instead of TagLib

--- Audio/Properties/MP3PropertiesReader.m
+++ Audio/Properties/MP3PropertiesReader.m
@@ -22,8 +22,32 @@
 #import "AudioStream.h"
 #include <mad/mad.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #define INPUT_BUFFER_SIZE		(5 * 8192)
 #define MPEG_FRAMES_TO_SCAN		32
+
+// From vbrheadersdk:
+// ========================================
+// A Xing header may be present in the ancillary
+// data field of the first frame of an mp3 bitstream
+// The Xing header (optionally) contains
+//      frames      total number of audio frames in the bitstream
+//      bytes       total number of bytes in the bitstream
+//      toc         table of contents
+
+// toc (table of contents) gives seek points
+// for random access
+// the ith entry determines the seek point for
+// i-percent duration
+// seek point in bytes = (toc[i]/256.0) * total_bitstream_bytes
+// e.g. half duration seek point = (toc[50]/256.0) * total_bitstream_bytes
+
+#define FRAMES_FLAG     0x0001
+#define BYTES_FLAG      0x0002
+#define TOC_FLAG        0x0004
+#define VBR_SCALE_FLAG  0x0008
 
 @implementation MP3PropertiesReader
 
@@ -58,16 +82,15 @@
 	
 	UInt32				channelsPerFrame		= 0;
 	Float64				sampleRate				= 0;
-	Float64				averageBitrate			= 0;
+	UInt32				bitrate					= 0;
+	UInt32				bitrateSum				= 0;
 	BOOL				isVBR					= NO;
 	
 	struct mad_stream	stream;
-	struct mad_header	header;
+	struct mad_frame	frame;
 	mad_timer_t			timer;
-	
-	int					result;
-	
-	//	struct stat			stat;
+		
+	struct stat			stat;
 
 	// Allocate the input buffer
 	inputBuffer = (unsigned char *)calloc(INPUT_BUFFER_SIZE + MAD_BUFFER_GUARD, sizeof(unsigned char));
@@ -106,15 +129,28 @@
 		return NO;
 	}
 	
-	//	result = fstat(fd, &stat);
-	//	NSAssert1(-1 != fd, @"Unable to stat the input file (%s).", strerror(errno));
+	int result = fstat(fd, &stat);
+	if(-1 == result) {
+		if(nil != error) {
+			NSMutableDictionary *errorDictionary = [NSMutableDictionary dictionary];
+			
+			[errorDictionary setObject:[NSString stringWithFormat:@"The file \"%@\" is not a valid MP3 file.", [path lastPathComponent]] forKey:NSLocalizedDescriptionKey];
+			[errorDictionary setObject:@"Not an MP3 file" forKey:NSLocalizedFailureReasonErrorKey];
+			[errorDictionary setObject:@"The file's extension may not match the file's type." forKey:NSLocalizedRecoverySuggestionErrorKey];						
+			
+			*error = [NSError errorWithDomain:AudioPropertiesReaderErrorDomain 
+										 code:AudioPropertiesReaderFileFormatNotRecognizedError 
+									 userInfo:errorDictionary];
+		}
+		
+		free(inputBuffer);
+		close(fd);
+		return NO;
+	}
 	
 	mad_stream_init(&stream);
-	mad_header_init(&header);
-//	mad_frame_init(&frame);
+	mad_frame_init(&frame);
 	mad_timer_reset(&timer);
-	
-	BOOL readEOF = NO;
 	
 	for(;;) {
 		if(NULL == stream.buffer || MAD_ERROR_BUFLEN == stream.error) {
@@ -163,7 +199,7 @@
 			stream.error = MAD_ERROR_NONE;
 		}
 		
-		result = mad_header_decode(&header, &stream);
+		result = mad_frame_decode(&frame, &stream);
 		
 		if(-1 == result) {
 			
@@ -196,19 +232,23 @@
 		}
 		
 		++framesDecoded;
-		mad_timer_add(&timer, header.duration);
+		mad_timer_add(&timer, frame.header.duration);
 		
 		// Look for a Xing header in the first frame that was successfully decoded
 		// Reference http://www.codeproject.com/audio/MPEGAudioInfo.asp
 		if(1 == framesDecoded) {
 			
-			sampleRate			= header.samplerate;
-			channelsPerFrame	= MAD_NCHANNELS(&header);
-			averageBitrate		= header.bitrate;
+			sampleRate			= frame.header.samplerate;
+			channelsPerFrame	= MAD_NCHANNELS(&frame.header);
+			bitrate				= frame.header.bitrate;
+			bitrateSum			= bitrate;
 			
+			[propertiesDictionary setValue:[NSNumber numberWithUnsignedInt:channelsPerFrame] forKey:@"channelsPerFrame"];
+			[propertiesDictionary setValue:[NSNumber numberWithUnsignedInt:sampleRate] forKey:@"sampleRate"];
+
 			uint32_t magic = mad_bit_read(&stream.anc_ptr, 32);
 			
-			switch(header.layer) {
+			switch(frame.header.layer) {
 				case MAD_LAYER_I:
 					[propertiesDictionary setValue:NSLocalizedStringFromTable(@"MPEG Audio", @"Formats", @"") forKey:@"fileType"];
 					[propertiesDictionary setValue:NSLocalizedStringFromTable(@"Layer I", @"Formats", @"") forKey:@"formatType"];
@@ -229,6 +269,7 @@
 				
 				unsigned	i;
 				uint32_t	frames;
+				uint32_t	bytes;
 				uint32_t	samplesPerMPEGFrame;
 				
 				uint32_t	flags = mad_bit_read(&stream.anc_ptr, 32);
@@ -236,15 +277,15 @@
 				// 4 byte value containing total frames
 				if(FRAMES_FLAG & flags) {					
 					// Determine number of samples, discounting encoder delay and padding
-					if(MAD_FLAG_LSF_EXT & header.flags || MAD_FLAG_MPEG_2_5_EXT & header.flags) {
-						switch(header.layer) {
+					if(MAD_FLAG_LSF_EXT & frame.header.flags || MAD_FLAG_MPEG_2_5_EXT & frame.header.flags) {
+						switch(frame.header.layer) {
 							case MAD_LAYER_I:		samplesPerMPEGFrame = 384;			break;
 							case MAD_LAYER_II:		samplesPerMPEGFrame = 1152;			break;
 							case MAD_LAYER_III:		samplesPerMPEGFrame = 576;			break;
 						}
 					}
 					else {
-						switch(header.layer) {
+						switch(frame.header.layer) {
 							case MAD_LAYER_I:		samplesPerMPEGFrame = 384;			break;
 							case MAD_LAYER_II:		samplesPerMPEGFrame = 1152;			break;
 							case MAD_LAYER_III:		samplesPerMPEGFrame = 1152;			break;
@@ -258,7 +299,7 @@
 				
 				// 4 byte value containing total bytes
 				if(BYTES_FLAG & flags) {
-					uint32_t bytes = mad_bit_read(&stream.anc_ptr, 32);
+					bytes = mad_bit_read(&stream.anc_ptr, 32);
 				}
 				
 				// 100 bytes containing TOC information
@@ -273,16 +314,18 @@
 				
 				// 4 byte value indicating encoded vbr scale
 				if(VBR_SCALE_FLAG & flags) {
-					uint32_t vbr_scale = mad_bit_read(&stream.anc_ptr, 32);
+					/*uint32_t vbr_scale =*/ mad_bit_read(&stream.anc_ptr, 32);
 				}
 				
-				mad_timer_add(&timer, header.duration);
+				mad_timer_add(&timer, frame.header.duration);
 				mad_timer_multiply(&timer, frames);
 				
 				framesDecoded	= frames;
 				float bitrate = 8.0 * (bytes / mad_timer_count(timer, MAD_UNITS_SECONDS));
 				[propertiesDictionary setValue:[NSNumber numberWithFloat:bitrate] forKey:@"bitrate"];
 				
+				[propertiesDictionary setValue:[NSNumber numberWithLong:mad_timer_count(timer, MAD_UNITS_SECONDS)] forKey:@"duration"];
+
 				// Loook for the LAME header next
 				// http://gabriel.mp3-tech.org/mp3infotag.html
 				magic = mad_bit_read(&stream.anc_ptr, 32);
@@ -295,48 +338,67 @@
 						versionString[i] = mad_bit_read(&stream.anc_ptr, 8);
 					}
 					
-					uint8_t infoTagRevision = mad_bit_read(&stream.anc_ptr, 4);
-					uint8_t vbrMethod = mad_bit_read(&stream.anc_ptr, 4);
+					/*uint8_t infoTagRevision =*/ mad_bit_read(&stream.anc_ptr, 4);
+					/*uint8_t vbrMethod =*/ mad_bit_read(&stream.anc_ptr, 4);
 					
-					uint8_t lowpassFilterValue = mad_bit_read(&stream.anc_ptr, 8);
+					/*uint8_t lowpassFilterValue =*/ mad_bit_read(&stream.anc_ptr, 8);
 					
-					float peakSignalAmplitude = mad_bit_read(&stream.anc_ptr, 32);
-					uint16_t radioReplayGain = mad_bit_read(&stream.anc_ptr, 16);
-					uint16_t audiophileReplayGain = mad_bit_read(&stream.anc_ptr, 16);
+					/*float peakSignalAmplitude =*/ mad_bit_read(&stream.anc_ptr, 32);
+					/*uint16_t radioReplayGain =*/ mad_bit_read(&stream.anc_ptr, 16);
+					/*uint16_t audiophileReplayGain =*/ mad_bit_read(&stream.anc_ptr, 16);
 					
-					uint8_t encodingFlags = mad_bit_read(&stream.anc_ptr, 4);
-					uint8_t athType = mad_bit_read(&stream.anc_ptr, 4);
+					/*uint8_t encodingFlags =*/ mad_bit_read(&stream.anc_ptr, 4);
+					/*uint8_t athType =*/ mad_bit_read(&stream.anc_ptr, 4);
 					
-					uint8_t lameBitrate = mad_bit_read(&stream.anc_ptr, 8);
+					/*uint8_t lameBitrate =*/ mad_bit_read(&stream.anc_ptr, 8);
 					
 					uint16_t encoderDelay = mad_bit_read(&stream.anc_ptr, 12);
 					uint16_t encoderPadding = mad_bit_read(&stream.anc_ptr, 12);
 					
 					[propertiesDictionary setValue:[NSNumber numberWithLongLong:(frames * samplesPerMPEGFrame) - (encoderDelay + encoderPadding)] forKey:@"totalFrames"];
 										
-					uint8_t misc = mad_bit_read(&stream.anc_ptr, 8);
+					/*uint8_t misc =*/ mad_bit_read(&stream.anc_ptr, 8);
 					
-					uint8_t mp3Gain = mad_bit_read(&stream.anc_ptr, 8);
+					/*uint8_t mp3Gain =*/ mad_bit_read(&stream.anc_ptr, 8);
 					
 					/*uint8_t garbage =*/mad_bit_read(&stream.anc_ptr, 2);
-					uint8_t surroundInfo = mad_bit_read(&stream.anc_ptr, 3);
-					uint16_t presetInfo = mad_bit_read(&stream.anc_ptr, 11);
+					/*uint8_t surroundInfo =*/ mad_bit_read(&stream.anc_ptr, 3);
+					/*uint16_t presetInfo =*/ mad_bit_read(&stream.anc_ptr, 11);
 					
-					uint32_t musicGain = mad_bit_read(&stream.anc_ptr, 32);
+					/*uint32_t musicGain =*/ mad_bit_read(&stream.anc_ptr, 32);
 					
-					uint32_t musicCRC = mad_bit_read(&stream.anc_ptr, 32);
+					/*uint32_t musicCRC =*/ mad_bit_read(&stream.anc_ptr, 32);
 					
-					uint32_t tagCRC = mad_bit_read(&stream.anc_ptr, 32);
+					/*uint32_t tagCRC =*/ mad_bit_read(&stream.anc_ptr, 32);
 				}
 				
 				break;
-				}			
-			}		
-		}
+			}
+			else {
+
+				// Don't waste time scanning the whole file
+				if(framesDecoded >= MPEG_FRAMES_TO_SCAN) {
+					if(isVBR) {
+						[propertiesDictionary setValue:[NSNumber numberWithFloat:(float)bitrateSum / framesDecoded] forKey:@"bitrate"];
+					}
+					else {
+						[propertiesDictionary setValue:[NSNumber numberWithFloat:bitrate] forKey:@"bitrate"];
+					}
+					break;
+				}
+				
+//				UInt32 currentSampleRate			= frame.header.samplerate;
+//				UInt32 currentChannelsPerFrame		= MAD_NCHANNELS(&frame.header);
+				UInt32 currentBitrate				= frame.header.bitrate;
+
+				if(currentBitrate != bitrate) {
+					isVBR = YES;
+				}
+			}
+		}		
 	}
 
 	// Clean up
-	mad_header_finish(&header);
 	mad_frame_finish(&frame);
 	mad_stream_finish(&stream);
 	
@@ -346,7 +408,6 @@
 	[self setValue:propertiesDictionary forKey:@"properties"];
 
 	return YES;
-
 }
 
 @end
