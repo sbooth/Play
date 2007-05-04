6756860b1c597e89fff90780a14400a3dc35fb52
stephen_booth <stephen_booth@localhost>
2007-05-04 02:12:14 +0000

    Handle 32-bit sample size (just in case)

--- Audio/Decoders/FLACStreamDecoder.m
+++ Audio/Decoders/FLACStreamDecoder.m
@@ -45,7 +45,7 @@ writeCallback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, cons
 	}
 	
 	float	*floatBuffer	= (float *)writePointer;
-	float	scaleFactor		= (1 << frame->header.bits_per_sample);
+	double	scaleFactor		= (1LL << frame->header.bits_per_sample);
 	
 	for(sample = 0; sample < frame->header.blocksize; ++sample) {
 		for(channel = 0; channel < frame->header.channels; ++channel) {
