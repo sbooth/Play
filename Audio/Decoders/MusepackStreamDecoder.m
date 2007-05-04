fa28a6b6db87201bb6d94b1c2874d36baaa4d2ad
stephen_booth <stephen_booth@localhost>
2007-05-04 02:31:10 +0000

    Use native float packed

--- Audio/Decoders/MusepackStreamDecoder.m
+++ Audio/Decoders/MusepackStreamDecoder.m
@@ -106,11 +106,11 @@
 
 	// Setup input format descriptor
 	_pcmFormat.mFormatID			= kAudioFormatLinearPCM;
-	_pcmFormat.mFormatFlags			= kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsBigEndian | kAudioFormatFlagIsPacked;
+	_pcmFormat.mFormatFlags			= kAudioFormatFlagsNativeFloatPacked;
 	
 	_pcmFormat.mSampleRate			= streaminfo.sample_freq;
 	_pcmFormat.mChannelsPerFrame	= streaminfo.channels;
-	_pcmFormat.mBitsPerChannel		= 16;
+	_pcmFormat.mBitsPerChannel		= 32;
 	
 	_pcmFormat.mBytesPerPacket		= (_pcmFormat.mBitsPerChannel / 8) * _pcmFormat.mChannelsPerFrame;
 	_pcmFormat.mFramesPerPacket		= 1;
@@ -137,25 +137,20 @@
 
 - (void) fillPCMBuffer
 {
-	UInt32				bytesToWrite, bytesAvailableToWrite;
-	UInt32				spaceRequired;
 	void				*writePointer;
 	MPC_SAMPLE_FORMAT	mpcBuffer [MPC_DECODER_BUFFER_LENGTH];
-	mpc_uint32_t		framesRead;
-	unsigned			sample;
 	
 	for(;;) {
-		bytesToWrite				= RING_BUFFER_WRITE_CHUNK_SIZE;
-		bytesAvailableToWrite		= [[self pcmBuffer] lengthAvailableToWriteReturningPointer:&writePointer];
-		spaceRequired				= MPC_FRAME_LENGTH * [self pcmFormat].mChannelsPerFrame * ([self pcmFormat].mBitsPerChannel / 8);	
+		UInt32	bytesToWrite			= RING_BUFFER_WRITE_CHUNK_SIZE;
+		UInt32	bytesAvailableToWrite	= [[self pcmBuffer] lengthAvailableToWriteReturningPointer:&writePointer];
+		UInt32	spaceRequired			= MPC_FRAME_LENGTH * [self pcmFormat].mChannelsPerFrame * ([self pcmFormat].mBitsPerChannel / 8);	
 		
 		if(bytesAvailableToWrite < bytesToWrite || spaceRequired > bytesAvailableToWrite) {
 			break;
 		}
 				
 		// Decode the data
-		framesRead		= mpc_decoder_decode(&_decoder, mpcBuffer, 0, 0);
-//		NSAssert((mpc_uint32_t)-1 != framesRead, NSLocalizedStringFromTable(@"Musepack decoding error.", @"Errors", @""));
+		mpc_uint32_t framesRead = mpc_decoder_decode(&_decoder, mpcBuffer, 0, 0);
 		if((mpc_uint32_t)-1 == framesRead) {
 			NSLog(NSLocalizedStringFromTable(@"Musepack decoding error.", @"Errors", @""));
 			return;
@@ -164,78 +159,16 @@
 #ifdef MPC_FIXED_POINT
 	#error "Fixed point not yet supported"
 #else
-		int32_t					audioSample			= 0;
-		int8_t					*alias8				= NULL;
-		int16_t					*alias16			= NULL;
-		int32_t					*alias32			= NULL;
-		int32_t					clipMin				= -1 << ([self pcmFormat].mBitsPerChannel - 1);
-		int32_t					clipMax				= (1 << ([self pcmFormat].mBitsPerChannel - 1)) - 1;
+		float		*floatBuffer	= (float *)writePointer;
+		float		audioSample		= 0;
+		unsigned	sample			= 0;
 		
-		switch([self pcmFormat].mBitsPerChannel) {
-			
-			case 8:
-				
-				// No need for byte swapping
-				alias8 = writePointer;
-				for(sample = 0; sample < framesRead * [self pcmFormat].mChannelsPerFrame; ++sample) {
-					audioSample		= mpcBuffer[sample] * (1 << 7);
-					audioSample		= (audioSample < clipMin ? clipMin : (audioSample > clipMax ? clipMax : audioSample));
-					*alias8++		= (int8_t)audioSample;
-				}
-					
-				[[self pcmBuffer] didWriteLength:framesRead * [self pcmFormat].mChannelsPerFrame * sizeof(int8_t)];
-				
-				break;
-				
-			case 16:
-				
-				// Convert to big endian byte order 
-				alias16 = writePointer;
-				for(sample = 0; sample < framesRead * [self pcmFormat].mChannelsPerFrame; ++sample) {
-					audioSample		= mpcBuffer[sample] * (1 << 15);
-					audioSample		= (audioSample < clipMin ? clipMin : (audioSample > clipMax ? clipMax : audioSample));
-					*alias16++		= (int16_t)OSSwapHostToBigInt16(audioSample);
-				}
-					
-				[[self pcmBuffer] didWriteLength:framesRead * [self pcmFormat].mChannelsPerFrame * sizeof(int16_t)];
-				
-				break;
-				
-			case 24:
-				
-				// Convert to big endian byte order 
-				alias8 = writePointer;
-				for(sample = 0; sample < framesRead * [self pcmFormat].mChannelsPerFrame; ++sample) {
-					audioSample		= mpcBuffer[sample] * (1 << 23);
-					audioSample		= (audioSample < clipMin ? clipMin : (audioSample > clipMax ? clipMax : audioSample));
-					audioSample		= OSSwapHostToBigInt32(audioSample);
-					*alias8++		= (int8_t)(audioSample >> 16);
-					*alias8++		= (int8_t)(audioSample >> 8);
-					*alias8++		= (int8_t)audioSample;
-				}
-					
-				[[self pcmBuffer] didWriteLength:framesRead * [self pcmFormat].mChannelsPerFrame * 3 * sizeof(int8_t)];
-				
-				break;
-				
-			case 32:
-				
-				// Convert to big endian byte order 
-				alias32 = writePointer;
-				for(sample = 0; sample < framesRead * [self pcmFormat].mChannelsPerFrame; ++sample) {
-					audioSample		= mpcBuffer[sample] * (1 << 31);
-					audioSample		= (audioSample < clipMin ? clipMin : (audioSample > clipMax ? clipMax : audioSample));
-					*alias32++		= OSSwapHostToBigInt32(audioSample);
-				}
-					
-				[[self pcmBuffer] didWriteLength:framesRead * [self pcmFormat].mChannelsPerFrame * sizeof(int32_t)];
-				
-				break;
-				
-			default:
-				@throw [NSException exceptionWithName:@"IllegalInputException" reason:@"Sample size not supported" userInfo:nil]; 
-				break;	
+		for(sample = 0; sample < framesRead * [self pcmFormat].mChannelsPerFrame; ++sample) {
+			audioSample		= mpcBuffer[sample];
+			*floatBuffer++	= (audioSample < -1.0 ? -1.0 : (audioSample > 1.0 ? 1.0 : audioSample));
 		}
+
+		[[self pcmBuffer] didWriteLength:framesRead * [self pcmFormat].mChannelsPerFrame * (32 / 8)];
 #endif /* MPC_FIXED_POINT */
 		
 		// EOS?
