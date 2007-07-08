5b4a0f55114de422d58542fb7a289b7322807232
stephen_booth <stephen_booth@localhost>
2007-07-08 02:39:13 +0000

    Moved to separate target

--- Scripts/generate_svn_revision.sh
+++
@@ -1,8 +0,0 @@
-## $Id$
-
-PATH="$PATH:/opt/local/bin:/usr/local/bin:/usr/local/subversion/bin:/sw/bin"
-REVISION=`svnversion .`
-
-## SVN_REVISION is a string because it may look like "4168M" or "4123:4168MS"
-echo "#define SVN_REVISION \"$REVISION\"" > $SCRIPT_OUTPUT_FILE_0
-echo "#define SVN_REVISION_UNQUOTED $REVISION" >> $SCRIPT_OUTPUT_FILE_0
